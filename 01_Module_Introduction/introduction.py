14851d063e50ae3b458f20dd2fd807da41297c08
Ahmad bala <www.ahmadbala194@gmail.com>
2024-12-14 01:00:45 +0100

    Update introduction.py

--- 01_Module_Introduction/introduction.py
+++ 01_Module_Introduction/introduction.py
@@ -1,1 +1,17 @@
+#some basic print operations
 
+print(3 +3)
+print(3-4)
+print(3/3)
+print(4 **2 )
+print(5 % 2)
+print(5 // 3)
+
+#checking some basics data types
+
+print(type(10)
+print(type(10.9)
+      
+      
+      
+      
