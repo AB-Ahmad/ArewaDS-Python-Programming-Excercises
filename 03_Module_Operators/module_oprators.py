53fadcb7a3c2ba1f33a60cfa50605ee1aeaaf68a
Ahmad bala <www.ahmadbala194@gmail.com>
2025-01-19 21:35:19 +0100

    Update module_oprators.py

--- 03_Module_Operators/module_oprators.py
+++ 03_Module_Operators/module_oprators.py
@@ -1,1 +1,23 @@
+#variable declaraton
+age = 24 
+height = 28.7
+complex_number = 2+ 3j
+
+print("Age",age,"Height",height,"complex_number",complex_number)
+
+# Calculate area of a triangle
+base = float(input("\nEnter base of the triangle: "))
+height = float(input("Enter height of the triangle: "))
+area_of_triangle = 0.5 * base * height
+#print(f"The area of the triangle is {area_of_triangle}")
+print("The area of the triangle is",area_of_triangle)
+
+
+# Calculate perimeter of a triangle
+a = float(input("\nEnter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+perimeter_of_triangle = a + b + c
+print("The perimeter of the triangle is ",perimeter_of_triangle)
+
 
