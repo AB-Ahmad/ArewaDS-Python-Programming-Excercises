82735a15853e0f530443f4a3d9c8ddcfcbc44f63
Ahmad bala <www.ahmadbala194@gmail.com>
2025-01-19 22:06:52 +0100

    Update module_oprators.py

--- 03_Module_Operators/module_oprators.py
+++ 03_Module_Operators/module_oprators.py
@@ -20,4 +20,36 @@ c = float(input("Enter side c: "))
 perimeter_of_triangle = a + b + c
 print("The perimeter of the triangle is ",perimeter_of_triangle)
 
+# Calculate area and perimeter of a rectangle
+length = float(input("\nEnter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area_of_rectangle = length * width
+perimeter_of_rectangle = 2 * (length + width)
+print(f"The area of the rectangle is " area_of_rectangle)
+print(f"The perimeter of the rectangle is",perimeter_of_rectangle)
+# calculate the slope of y = 2x-2
+#by difrentiation to get my slope whc=ich isequal to 2
+m= 2 # slope
+print("slope = ",m )
+x-intercepts = 1 # when y = 0
+y-intercepts = -2 # when x = 0
+
+#slope and euclidean distance
+# m= y2=y1/x2-x1 and point is (2,2) and (6,10)
+x1,y1,x2,y2 = 2,2,6,10
+m2 = (y2-y1)(x2-x1)
+e_distance = ((x2-x1)**2 + (y2-y1)**2)**0.5
+print("The slope is",m2)
+print("The euclidean ditance is"e_distance)
+
+#comaprarison of the two previous calculated slopes
+print("comparison of the 2 slopes", m == m2)
+
+
+
+
+
+
+
+
 
