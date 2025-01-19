434ed6994778e843fa653faf6c9c8d03a0bc5f6d
Ahmad bala <www.ahmadbala194@gmail.com>
2025-01-19 20:44:36 +0100

    Update 02_variables_builtin_functions.py

--- 02_Module_Variables_Buildin_functions/02_variables_builtin_functions.py
+++ 02_Module_Variables_Buildin_functions/02_variables_builtin_functions.py
@@ -1,1 +1,16 @@
+# DAY 2 : 30 DAYS OF PYTHON PROGRAMMING
+ 
+FIRST_NAMME = "Ahmad"
+LAST_NAME = "BALA"
+FULL_NAME = "Ahmad Bala"
+COUNTRY = "Nigeria"
+CITY = "Joa"
+age = 24
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
 
+
+#A multiply variable declare in a single line
+Name, state_origin,my_age = "Ahmad","Kano",24
