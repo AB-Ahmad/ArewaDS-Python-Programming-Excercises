f1c0bbb7121bfbd618e4b920a1f96c1a62df6d6d
Ahmad bala <www.ahmadbala194@gmail.com>
2025-01-19 21:06:47 +0100

    Update 02_variables_builtin_functions.py

--- 02_Module_Variables_Buildin_functions/02_variables_builtin_functions.py
+++ 02_Module_Variables_Buildin_functions/02_variables_builtin_functions.py
@@ -14,3 +14,37 @@ is_light_on = True
 
 #A multiply variable declare in a single line
 Namee, state_origin,my_age = "Ahmad","Kano",24
+
+#checking the dtata types of this variables
+print(type(FIRST_NAMME))
+print(type(LAST_NAME))
+print(type(FULL_NAME))
+print(type(COUNTRY))
+print(type(CITY))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+#lenght of my first name
+print(len(FIRST_NAME))
+#COMPARING THE LENGTH OF FIRST NAME AND LAST NAME
+print("The length of first name is",len(FIRST_NAME), "while the length of last name is ",len(LAST_NAME))
+
+
+num_one = 5
+num_two = 4
+
+# Arithmetic operation
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+#or uing
+# floor_division = float
+
+
