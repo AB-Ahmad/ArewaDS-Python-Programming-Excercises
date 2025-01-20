cbe63965b7e986b0e5bc1e616ff8d469f6fbe586
Ahmad bala <www.ahmadbala194@gmail.com>
2025-01-20 10:58:52 +0100

    Update 04_strings.py

--- 04_Module_Strings/04_strings.py
+++ 04_Module_Strings/04_strings.py
@@ -1,1 +1,24 @@
+# Concatenate strings
+print('Thirty' + ' ' + 'Days' + ' ' + 'Of' + ' ' + 'Python')
+print('Coding' + ' ' + 'For' + ' ' + 'All')
 
+# Declare variable
+company = "Coding For All"
+print(company)
+print(len(company))
+
+print(company.upper)
+print(company.lower)
+
+print(capatilze(company))
+print(title(comapny))
+print(swapcase(comapny))
+
+#slice 
+print(comapny[0:6))
+
+#check coding incompany variable
+
+print("is coding contain in  company varibale: ", "coding" in company)
+#using find fuction
+print(company.find('Coding'))
